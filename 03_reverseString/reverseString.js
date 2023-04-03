d45cecd3916d89e3fb3b7492010aca410d9d94a0
Skela0312 <aleksander.lankarani@googlemail.com>
2023-04-03 23:12:55 +0200

    tasks1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    if (str === "") {
+        return ""
+    } 
+    let reversedArray = [];
+    let charArr = str.split("");
+    // console.log(charArr);
+    for (let i = 0; i < str.length; i++) {
+        reversedArray.push(charArr.pop());
+        // console.log(reversedArray);
+    }
+    let reversedString = reversedArray.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
