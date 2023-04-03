e26d90cb67386f472d07c11455c23f4285794597
Skela0312 <aleksander.lankarani@googlemail.com>
2023-04-04 00:01:57 +0200

    tasks4-5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    let parameters = arguments.length; // 2
+    for (let i = 1; i < parameters; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] === arguments[i]) {
+                arr.splice(j, 1);
+            } 
+        }
+    }
+    return arr;
+        
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    // if either of them is not a number
+    if (typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR";
+    // if either of them is negative
+    } else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    // if num1 is smaller than num2, swap them
+    } else if (num1 > num2) {  
+        let tmp = num1;
+        num1 = num2;
+        num2 = tmp;
+    }
+    let sum = 0;
+    for (i = num1; i <= num2; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
